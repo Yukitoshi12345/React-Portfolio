2184441b80f5f8fd1ed43d3c1325598d4b600cb3
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-05-12 22:32:47 +1000

    added logos

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,56 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "script-defer",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Yukitoshi Portfolio",
+        short_name: "Yukitoshi Imaizumi-Zhou",
+        description: "Yukitoshi's Portfolio made with React Vite",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,png,svg,jpg,pdf,jsx}"],
+        runtimeCaching: [
+          {
+            urlPattern: ({ url }) => {
+              return url.pathname.startsWith("/src/assets");
+            },
+            handler: "CacheFirst",
+            options: {
+              cacheName: "assets-cache",
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
+  server: {
+    port: 3000,
+    open: true,
+    host: true,
+  },
+});
