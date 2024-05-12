06174dbefc1e412deb08bd247243c7716540c797
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-05-12 22:48:46 +1000

    updated vite.config.js:

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
+
+let logoURL = "/logo-black.svg";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -18,12 +21,12 @@ export default defineConfig({
         theme_color: "#ffffff",
         icons: [
           {
-            src: "pwa-192x192.png",
+            src: "logoURL",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "pwa-512x512.png",
+            src: "logoURL",
             sizes: "512x512",
             type: "image/png",
           },
