ef4e019a413fe9127c7d8567267ed5cdcdaf810a
Yukitoshi <yukitoshi.imaizumizhou@gmail.com>
2024-06-06 01:44:44 +1000

    nav bar pretty well done for now

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,8 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 
 function Header() {
   const menuList = [
@@ -27,13 +29,29 @@ function Header() {
 
   return (
     <AppBar position="static">
-      <Container>
-        <Toolbar>
-          {menuList.map((item) => (
-            <Button key={item.id} color="inherit">
-              <Typography variant="h6">{item.title}</Typography>
-            </Button>
-          ))}
+      <Container maxWidth="lg">
+        <Toolbar disableGutters>
+          <Grid container alignItems="center" justifyContent="space-between">
+            <Grid item>
+              <Typography
+                variant="h6"
+                noWrap
+                component="div"
+                sx={{ fontWeight: "bold", letterSpacing: 1.2 }}
+              >
+                Yukitoshi Imaizumi-Zhou
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Box sx={{ display: "flex" }}>
+                {menuList.map((item) => (
+                  <Button key={item.id} color="inherit">
+                    <Typography variant="h6">{item.title}</Typography>
+                  </Button>
+                ))}
+              </Box>
+            </Grid>
+          </Grid>
         </Toolbar>
       </Container>
     </AppBar>
