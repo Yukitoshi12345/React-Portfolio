cdc36e46b4f7f9ce857d026e2cda7b7cfea08f33
Yukitoshi <yukitoshi.imaizumizhou@gmail.com>
2024-06-06 01:01:01 +1000

    created and completed error page

--- vite.config.js
+++ vite.config.js
@@ -1,59 +1,48 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
-let logoURL = "/logo-black.svg";
+let faviconURL = "/logo-black.svg";
 
-// https://vitejs.dev/config/
+const manifestForPlugIn = {
+  includeAssets: [faviconURL],
+  manifest: {
+    theme_color: "#ffffff",
+    icons: [
+      {
+        src: faviconURL,
+        sizes: "512x512",
+        type: "image/svg+xml",
+        purpose: "any maskable",
+      },
+      {
+        src: faviconURL,
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  },
+  workbox: {
+    globPatterns: ["**/*.{js,css,html,png,svg,jpg,pdf,jsx}"],
+    runtimeCaching: [
+      {
+        urlPattern: ({ url }) => url.pathname.startsWith("/src/assets"),
+        handler: "CacheFirst",
+        options: {
+          cacheName: "assets-cache",
+          cacheableResponse: {
+            statuses: [0, 200], // Caches successful and opaque responses
+          },
+        },
+      },
+    ],
+  },
+};
+
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      injectRegister: "script-defer",
-      devOptions: {
-        enabled: true,
-      },
-      manifest: {
-        name: "Yukitoshi Portfolio",
-        short_name: "Yukitoshi Imaizumi-Zhou",
-        description: "Yukitoshi's Portfolio made with React Vite",
-        theme_color: "#ffffff",
-        icons: [
-          {
-            src: "logoURL",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "logoURL",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-      workbox: {
-        globPatterns: ["**/*.{js,css,html,png,svg,jpg,pdf,jsx}"],
-        runtimeCaching: [
-          {
-            urlPattern: ({ url }) => {
-              return url.pathname.startsWith("/src/assets");
-            },
-            handler: "CacheFirst",
-            options: {
-              cacheName: "assets-cache",
-              cacheableResponse: {
-                statuses: [0, 200],
-              },
-            },
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react(), VitePWA(manifestForPlugIn)],
   server: {
     port: 3000,
     open: true,
-    host: true,
   },
 });
