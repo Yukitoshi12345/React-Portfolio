b9953331bb62a87edfbdb1169bf46ad4b3101305
Yukitoshi <yukitoshi.imaizumizhou@gmail.com>
2024-06-06 01:59:13 +1000

    created most of footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 // import { useState } from "react";
 
-import "./App.css";
-import Header from "./components/Header";
-import Hero from "./components/Hero";
+import './App.css';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Footer from './components/Footer';
 
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
-    <div>
+    <div
+      style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}
+    >
       <Header />
-      <Hero />
+      <div style={{ flexGrow: 1 }}>
+        <Hero />
+      </div>
+      <Footer />
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,29 @@
 // import React from "react";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import Container from "@mui/material/Container";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 
 function Header() {
   const menuList = [
     {
       id: 1,
-      title: "About Me",
+      title: 'About Me',
     },
     {
       id: 2,
-      title: "Portfolio",
+      title: 'Portfolio',
     },
     {
       id: 3,
-      title: "Contact",
+      title: 'Contact',
     },
     {
       id: 4,
-      title: "Resume",
+      title: 'Resume',
     },
   ];
 
@@ -37,13 +37,13 @@ function Header() {
                 variant="h6"
                 noWrap
                 component="div"
-                sx={{ fontWeight: "bold", letterSpacing: 1.2 }}
+                sx={{ fontWeight: 'bold', letterSpacing: 1.2 }}
               >
                 Yukitoshi Imaizumi-Zhou
               </Typography>
             </Grid>
             <Grid item>
-              <Box sx={{ display: "flex" }}>
+              <Box sx={{ display: 'flex' }}>
                 {menuList.map((item) => (
                   <Button key={item.id} color="inherit">
                     <Typography variant="h6">{item.title}</Typography>
