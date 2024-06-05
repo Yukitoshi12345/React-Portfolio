a547bc390b92beb0b9894e1d44616423c503932f
Yukitoshi <yukitoshi.imaizumizhou@gmail.com>
2024-06-06 01:48:38 +1000

    added prettier and eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import { useState } from "react";
+
 import "./App.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <div>
