31c1c274578f538e5da4cf38c2376fcdc79edae5
Yukitoshi <yukitoshi.imaizumizhou@gmail.com>
2024-06-09 01:21:29 +1000

    completed Main.jsx

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import Homepage from "./Homepage";
+
+const Main = () => {
+  return (
+    <div className="font-roboto bg-white-800 min-h-screen">
+      <Homepage />
+    </div>
+  );
+};
+
+export default Main;
