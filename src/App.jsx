2ebd528cc48e35aa66e401705a70125ca52bd52d
Yukitoshi <yukitoshi.imaizumizhou@gmail.com>
2024-06-09 01:19:51 +1000

    tidying up

---
+++ src/App.jsx
@@ -0,0 +1,98 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav';
+import Main from './components/Main.jsx';
+import Contact from './components/pages/Contact';
+import Resume from './components/pages/Resume.jsx';
+import Portfolios from './components/pages/Portfolio.jsx';
+import Footer from './components/pages/Footer';
+import React, { useEffect, useState } from 'react';
+import dotenv from 'dotenv';
+
+export default function App() {
+  const [theme, setTheme] = useState(null);
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+      setTheme(savedTheme);
+    } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    }
+  }, []);
+
+  const handleThemeSwitch = () => {
+    const newTheme = theme === 'dark' ? 'light' : 'dark';
+    setTheme(newTheme);
+    localStorage.setItem('theme', newTheme);
+  };
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [theme]);
+
+  const sun = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+      />
+    </svg>
+  );
+
+  const moon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="white"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+      />
+    </svg>
+  );
+
+  return (
+    <Router>
+      <button
+        type="button"
+        onClick={handleThemeSwitch}
+        className="fixed p-1 z-10 right-1 top-1 bg-gradient-to-r from-purple-400 via-indigo-400 to-blue-400 text-white dark:bg-orange-300 dark:via-transparent dark:from-transparent dark:to-transparent text-lg rounded-md"
+        aria-label={`Switch to ${theme === 'dark' ? 'light' : 'dark'} mode`}
+      >
+        {theme === 'dark' ? sun : moon}
+      </button>
+      <div className="flex flex-col min-h-screen">
+        <Nav />
+        <div className="flex-grow bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 min-h-screen font-inter">
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/home" element={<Main />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/resume" element={<Resume />} />
+            <Route path="/portfolios" element={<Portfolios />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
