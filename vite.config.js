8dec8f74796b0cdfb11c02f3b82a905d83bd39a8
Yukitoshi <yukitoshi.imaizumizhou@gmail.com>
2024-06-10 02:36:44 +1000

    app installable test

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,48 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
+let faviconURL = '/react.svg';
+
+const manifestForPlugIn = {
+  includeAssets: [faviconURL],
+  manifest: {
+    theme_color: '#ffffff',
+    icons: [
+      {
+        src: faviconURL,
+        sizes: '512x512',
+        type: 'image/svg+xml',
+        purpose: 'any maskable',
+      },
+      {
+        src: faviconURL,
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  },
+  workbox: {
+    globPatterns: ['**/*.{js,css,html,png,svg,jpg,pdf,jsx}'],
+    runtimeCaching: [
+      {
+        urlPattern: ({ url }) => url.pathname.startsWith('/src/assets'),
+        handler: 'CacheFirst',
+        options: {
+          cacheName: 'assets-cache',
+          cacheableResponse: {
+            statuses: [0, 200], // Caches successful and opaque responses
+          },
+        },
+      },
+    ],
+  },
+};
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), VitePWA(manifestForPlugIn)],
+  server: {
+    port: 3000,
+    open: true,
+  },
+});
