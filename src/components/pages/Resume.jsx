c81df52c02e93c27cd39f0303437cad0147b1e16
Yukitoshi <yukitoshi.imaizumizhou@gmail.com>
2024-06-25 23:25:42 +1000

    fixed font for technical proficiencies

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -272,11 +272,11 @@ const Resume = () => {
   };
 
   return (
-    <div
-      className="font-roboto mx-auto my-10 max-w-4xl p-8 rounded-lg shadow-lg bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 text-center"
-      style={{ fontFamily: 'math' }}
-    >
-      <h1 className="text-4xl font-bold text-indigo-600 dark:text-indigo-400 mb-6">
+    <div className="font-roboto mx-auto my-10 max-w-4xl p-8 rounded-lg shadow-lg bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 text-center">
+      <h1
+        className="text-4xl font-bold text-indigo-600 dark:text-indigo-400 mb-6"
+        style={{ fontFamily: 'math' }}
+      >
         My Technical Proficiencies
       </h1>
 
